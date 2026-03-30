ff4a426906b8a2cb41e3f5a62b1db1d1a2fc5c5b
Mithila001 <mithiladissanayakaofficial@gmail.com>
2026-03-30 21:07:56 +0530

    code cleaned upped

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import InputPlan from "./pages/InputPlan";
-import ConfigureRooms from "./pages/ConfigureRooms";
 import Caves from "./pages/Caves";
 
 function App() {
@@ -12,10 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
           <Route path="caves" element={<Caves />} />
-          <Route path="input-plan" element={<InputPlan />} />
-          <Route path="configure-rooms" element={<ConfigureRooms />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Route>
